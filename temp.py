f295ca1b10aa99533d63f60f7097b9b5e912d56d
alejos4n <alejo-lopera@outlook.com>
2020-04-29 02:44:28 -0500

    primer archivo de python en git

---
+++ temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+A = {1, 2, 3, 7, 9, 11}
+B = {3, 7, 17, 3, 7}
+
+print("Elementos del conjunto |A| ", len(A))
+print(A.intersection(B))
