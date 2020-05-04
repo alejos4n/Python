e2da4260a1628c1d1e0c99e8023ec4242df17235
alejos4n <alejo-lopera@outlook.com>
2020-05-03 23:58:21 -0500

    agrego grid y nombres de ejes

--- clase2_caldif.py
+++ clase2_caldif.py
@@ -4,6 +4,9 @@
 Created on Sun May  3 23:37:55 2020
 
 @author: alejo
+#función creada en la clase de cálculo.
+#autor: Profesor Rafael Montoya.
+
 """
 
 #función creada en la clase de cálculo.
@@ -15,10 +18,14 @@ Created on Sun May  3 23:37:55 2020
 import numpy as np
 import matplotlib.pyplot as plt
 
-#(0, 15, la grafica en x va de 0 a 15)
+#(0, 15, la grafica en eje x va de 0 a 15)
 #100 los puntos
 
 x = np.linspace(0, 15, 100)
 y = np.sin(x)
 
 plt.plot(x, y)
+
+plt.grid()
+plt.xlabel("x")
+plt.ylabel("f(x) = sin x")
