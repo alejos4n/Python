a1e842e9d18a5aec89ab591e1eaad7aebbe49614
alejos4n <alejo-lopera@outlook.com>
2020-05-03 23:08:12 -0500

    clase 1

---
+++ clase1_caldif.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 22:46:53 2020
+
+@author: alejo
+"""
+
+#función creada en la clase de cálculo.
+#autor: Profesor Rafael Montoya.
+
+def f(x):
+    return x ** 2 - 3 * x + 1
+
+print(f(-1))
+
+
