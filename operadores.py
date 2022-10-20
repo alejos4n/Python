7fde8534c3b028df676410cc368e44df53b48c26
alejos4n <alejo-lopera@outlook.com>
2022-10-20 00:39:21 -0500

    operadores en python

--- operadores.py
+++ operadores.py
@@ -6,6 +6,12 @@ Created on Wed Sep  1 00:52:31 2021
 @author: alejo
 """
 
+print("suma en python es 2 + 2 = ", 2 +2)
+print("resta en python es 8 - 5 = ", 8 - 5)
+print("multiplicacion en python es 2 * 3 =", 2 * 3)
+print("division en python es 75 / 9 = ", 75 / 9)
+print("potenciacion en python es 2 ** 3 = ", 2 ** 3)
+
 num1 = int(input("Ingresa el primer número: "))
 num2 = int(input("Ingresa el segundo número: "))
 num3 = int(input("Ingresa el tercer número: "))
