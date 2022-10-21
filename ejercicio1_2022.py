26ea1f564dffa0fd74c8d668b92dae012a4a9d0c
alejos4n <alejo-lopera@outlook.com>
2022-10-21 01:52:50 -0500

    ejercicios 1 y 2

---
+++ ejercicio1_2022.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed oct 18 23:32:16 2021
+
+@author: alejo
+"""
+
+num1 = 3
+num2 = 2
+num3 = 2
+num4 = 5
+
+potenciacion =((num1 + num2)/(num3 * num4)) ** 2
+
+print(potenciacion)
---
+++ ejercicio2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 18 23:32:16 2021
+
+@author: alejo
+"""
+
+peso_payaso = 112
+peso_muneca = 75
+pedido_payaso = 23
+pedido_muneca = 54
+
+peso_total_payaso = peso_payaso * pedido_payaso
+peso_total_muneca = peso_muneca * pedido_muneca
+
+peso_total = peso_total_muneca + peso_total_payaso
+
+print("el peso total de la venta es ", peso_total)
