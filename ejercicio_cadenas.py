a442eb2f73cf50e0d537c55e316e6b36fdb430b6
alejos4n <alejo-lopera@outlook.com>
2022-10-22 02:19:03 -0500

    trabajando con trings

---
+++ ejercicio_cadenas.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 22 00:05:09 2021
+
+@author: alejo
+"""
+
+cadena1 = "Te quiero solo como amigo"
+
+print(cadena1)
+print("Imprimo los dos primeros caracteres")
+print(cadena1[:2])
+print("Imprimo los tres ultimos caracteres")
+print(cadena1[-3])
+print(cadena1.strip(2))
+
+
---
+++ string.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri oct 22 00:05:09 2021
+
+@author: alejo
+"""
+
+cadena = "esto es un ejemplo de cadena"
+print(cadena)
+
+nombre = "alejandro"
+apellido = "sanchez"
+
+print(nombre + apellido)
+
+print(type(str(apellido)))
+
+cad = "este es un ejemplo de substring"
+print(len(cad))
+print(cad[5])
+print(cad[5:15])
+print(cad[:20])
+print(cad)
+
+#metodos
+
+metodo = 'Estoy usando los metodos de Python'
+
+print(metodo)
+print(metodo.lower())
+print(metodo.split())
+print(metodo.capitalize())
+print(metodo.title())
+print(metodo.swapcase())
+
+
+
