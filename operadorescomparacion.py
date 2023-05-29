bf90556ed0377db9fa1b24c877e0244fd53b1204
alejos4n <alejo-lopera@outlook.com>
2023-05-28 22:58:07 -0500

    operadors

---
+++ operadorescomparacion.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  9 22:11:46 2021
+
+@author: alejo
+"""
+
+num1 = 1000
+num2 = 500
+num3 = 1000
+
+cadena1 = "hola cadena"
+cadena2 = "hola cadena 2"
+
+print (num1 > num2)
+print (num1 == num3)
+print (num2 != num1)
+print (cadena1 > cadena2)
