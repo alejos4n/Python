a3833c72502b97958b133916d36a7b3f493fbb8a
alejos4n <alejo-lopera@outlook.com>
2023-05-31 01:06:48 -0500

    metdos listas

--- metodoslistas.py
+++ metodoslistas.py
@@ -5,3 +5,12 @@ Created on Sat Oct  2 21:55:00 2021
 
 @author: alejo
 """
+
+
+listanumeros = [1, 2, 3, 4, 5, 6]
+print("imprimo la lista inicial ", listanumeros)
+
+print("agrego el 7 a la lista")
+listanumeros.append(7)
+print("imprimo la lista con el 7 agregado")
+print(listanumeros)
