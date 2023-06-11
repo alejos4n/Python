a5bea9050ff20abcafd25cd0f55b572a301cbdd3
alejos4n <alejo-lopera@outlook.com>
2023-06-11 00:44:01 -0500

    parametros y argumentos

--- argumentosyparametros.py
+++ argumentosyparametros.py
@@ -5,3 +5,17 @@ Created on Sun Oct  3 20:37:11 2021
 
 @author: alejo
 """
+
+#parametro: variable a utilizar cuando se crea la funcion
+#argumento: valor que se le asigna a un parametro
+
+#creo la funcion suma
+def suma():
+    num1 = 20
+    num2 = 30
+    suma = num1 + num2
+    return suma
+
+print(suma())
+
+#en le codigo anterior cada vez que llamo la funcion suma(), viene con esas variables
