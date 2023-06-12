7c4222d41de1606a648b48eee7ff8477e07860d8
alejos4n <alejo-lopera@outlook.com>
2023-06-12 03:34:16 -0500

    POO en python, atributos

--- POO/clasesyobjetos.py
+++ POO/clasesyobjetos.py
@@ -5,3 +5,14 @@ Created on Sat Oct  2 23:44:12 2021
 
 @author: alejo
 """
+#creo una clase
+class FabricaTelefono():
+    marca = "Xiaomi"
+    color = "Negro"
+    ram = 4
+    alma = 64
+
+#Asigno la clase a una variable
+telefono = FabricaTelefono()
+#muestro un atributo, en este caso la marca
+print(telefono.marca)
