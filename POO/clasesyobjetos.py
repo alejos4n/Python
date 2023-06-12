8805df323e14b5ed0d74ae32d0eda77964d8286d
alejos4n <alejo-lopera@outlook.com>
2023-06-12 03:41:56 -0500

    metodos en clases

--- POO/clasesyobjetos.py
+++ POO/clasesyobjetos.py
@@ -7,12 +7,18 @@ Created on Sat Oct  2 23:44:12 2021
 """
 #creo una clase
 class FabricaTelefono():
+    #creo los atributos
     marca = "Xiaomi"
     color = "Negro"
     ram = 4
     alma = 64
 
+    #creo el meotodo, metodo = funcion, solo cambia que esta en una clase
+    def llamar(self, mensaje):
+        return mensaje
+
 #Asigno la clase a una variable
 telefono = FabricaTelefono()
 #muestro un atributo, en este caso la marca
 print(telefono.marca)
+print(telefono.llamar("Hola, mesanje desde el metodo llamar de la clase FabricateTelefono"))
