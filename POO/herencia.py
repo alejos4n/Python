d65015ad8f950703a8a4e4ef1b2b7f678f75954b
alejos4n <alejo-lopera@outlook.com>
2023-07-06 23:45:10 -0500

    herencia en python

--- POO/herencia.py
+++ POO/herencia.py
@@ -5,3 +5,24 @@ Created on Sat Oct  2 23:44:12 2021
 
 @author: alejo
 """
+
+class Animales():
+    def habla(self):
+        print("Yo soy un animal")
+    
+    def descripcion(self):
+        print("yo soy un {}".format(self.animal))
+
+class Perro(Animales):
+    pass
+
+class Abeja(Animales):
+    def __init__(self, animal):
+        self.animal = animal
+
+animal = Animales()
+animal.habla()
+perro = Perro()
+perro.habla()
+abeja = Abeja("Abeja")
+abeja.descripcion()
